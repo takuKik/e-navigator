3bfec60194e7982c7d234df49342ce3d908eaa4c
Takuya KIkuchi <ta.ku.ya7617@gmail.com>
2019-03-14 10:49:15 +0900

    create interview action and view

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,2 +1,25 @@
 class InterviewsController < ApplicationController
+  def index
+    @interviews = current_user.interviews.order("interview_date DESC")
+  end
+
+  def new
+    @user = current_user
+    @interview = Interview.new
+  end
+
+  def create
+    @user = current_user
+    @interview = Interview.new(interview_params)
+    if @interview.save
+      redirect_to user_interviews_url
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def interview_params
+    params.require(:interview).permit(:interview_date).merge(user_id: params[:user_id])
+  end
 end
