8b8b8b8dbaf5470dd7d44ec816f2b990ecd6af52
Takuya KIkuchi <ta.ku.ya7617@gmail.com>
2019-03-14 13:36:33 +0900

    fix controller

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,47 +1,49 @@
 class InterviewsController < ApplicationController
+  before_action :set_user, except: :destroy
+  before_action :set_interview, only: [:edit, :update, :destroy]
+
   def index
-    @user = current_user
     @interviews = current_user.interviews.order("interview_date DESC")
   end
 
   def new
-    @user = current_user
     @interview = Interview.new
   end
 
   def create
-    @user = current_user
     @interview = Interview.new(interview_params)
     if @interview.save
-      redirect_to user_interviews_url
+      redirect_to user_interviews_url, notice: "新規面接日程を作成しました。"
     else
-      render 'new'
+      render action: :new
     end
   end
 
   def edit
-    @user = current_user
-    @interview = Interview.find(params[:id])
   end
 
   def update
-    @user = current_user
-    @interview = Interview.find(params[:id])
     if  @interview.update(interview_params)
-      redirect_to user_interviews_url
+      redirect_to user_interviews_url, notice: "面接日時を更新しました。"
     else
-      render 'edit'
+      render action: :edit
     end
   end
 
   def destroy
-    @user = current_user
-    @interview = Interview.find(params[:id])
     @interview.destroy
-    redirect_to user_interviews_url
+    redirect_to user_interviews_url, notice: "面接日時を削除しました。"
   end
 
   private
+  def set_user
+    @user = current_user
+  end
+
+  def set_interview
+    @interview = Interview.find(params[:id])
+  end
+
   def interview_params
     params.require(:interview).permit(:interview_date).merge(user_id: params[:user_id])
   end
