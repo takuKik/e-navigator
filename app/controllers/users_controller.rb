ea6bdd8d3ae0a8d11bc5e777344264415bff1943
Takuya KIkuchi <ta.ku.ya7617@gmail.com>
2019-04-01 11:00:41 +0900

    Display a list of users other than yourself

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update]
   def index
-    @users = User.all
+    @users = User.where.not(id: current_user.id)
   end
 
   def edit
